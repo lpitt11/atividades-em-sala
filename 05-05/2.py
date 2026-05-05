ea57ac78a30deb069eb88398f2f9a6cd6bd14cd1
lpitt11 <ispittw@gmail.com>
2026-05-05 11:39:27 -0300

    atv

--- 05-05/2.py
+++ 05-05/2.py
@@ -3,4 +3,4 @@ n = int(input("Digite o valor de n: "))
 for i in range(n):
     for j in range(n):
         print("X", end=" ")
-    print() # Pula para a próxima linha após completar as colunas
+    print()
